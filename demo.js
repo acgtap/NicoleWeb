af1987d3e62919a66b2a74111b7bbba8ec3f80b4
林檎 <40010177+acgtap@users.noreply.github.com>
2020-06-24 22:43:24 +0800

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,23 @@
+
+
+initPlayers();
+
+
+
+function initPlayers() {
+    const dp = new DPlayer({
+        container: document.getElementById('dplayer'),
+        autoplay: true,
+        theme: '#FADFA3',
+        loop: true,
+        lang: 'zh-cn',
+        screenshot: true,
+        hotkey: true,
+        preload: 'auto',
+        video: {
+            url: 'https://linqin-1252236879.file.myqcloud.com/VID_20191114_171850_9.mp4',
+        },
+    });
+
+
+}
